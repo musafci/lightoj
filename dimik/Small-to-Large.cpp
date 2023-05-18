a5536cb86a4accd0ce51829e0c27cb1cc95bbd8b
Md. Abu Musa <musa.fci@gmail.com>
2023-05-18 22:03:58 +0600

    Small-to-Large [Solution #02]

--- dimik/Small-to-Large.cpp
+++ dimik/Small-to-Large.cpp
@@ -34,3 +34,40 @@ int main() {
     }    
     return 0;
 }
+
+
+// Solution #02:
+
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++) {
+        int n1, n2, n3;
+        cin >> n1 >> n2 >> n3;
+
+        // Create an array with the three numbers
+        int numbers[] = {n1, n2, n3};
+
+        // Sort the array in ascending order
+        sort(numbers, numbers + 3);
+
+        // Print the case number
+        cout << "Case " << i << ": ";
+
+        // Print the sorted numbers
+        for (int j = 0; j < 3; j++) {
+            cout << numbers[j] << " ";
+        }
+
+        // Print a new line after each case
+        cout << endl;
+    }
+
+    return 0;
+}
